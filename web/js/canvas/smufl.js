2315207ad6204a79a191c9cb18607a309a5aad36
John Rothfield <johnrothfield@gmail.com>
2025-06-09 22:17:18 -0600

    music fonts

---
+++ web/js/canvas/smufl.js
@@ -0,0 +1,20 @@
+// smufl.js
+// Bravura glyphs from the SMuFL Private Use Area (PUA)
+
+export const SMuFL = {
+  accidentalSharp: "\uE262",
+  accidentalFlat: "\uE260",
+  accidentalNatural: "\uE261",
+  doubleSharp: "\uE263",
+  doubleFlat: "\uE264",
+  barlineSingle: "\uE030",
+  barlineDouble: "\uE031",
+  barlineFinal: "\uE032",
+  breathMarkComma: "\uE4CE",
+  caesura: "\uE4D0",
+  fermataAbove: "\uE4C0",
+  repeatStart: "\uE040",
+  repeatEnd: "\uE041",
+  segno: "\uE047",
+  coda: "\uE048"
+};
